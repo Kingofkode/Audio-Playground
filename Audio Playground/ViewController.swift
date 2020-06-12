7efaa6eb303dd53b68ba9a15db5c61b8727a77cd
Isaiah Suarez <isaiah.suarez@icloud.com>
2020-06-12 11:56:12 -0500

    Initial Commit

---
+++ Audio Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Audio Playground
+//
+//  Created by Isaiah Suarez on 6/12/20.
+//  Copyright © 2020 Isaiah Suarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Audio PlaygroundTests/Audio_PlaygroundTests.swift
@@ -0,0 +1,34 @@
+//
+//  Audio_PlaygroundTests.swift
+//  Audio PlaygroundTests
+//
+//  Created by Isaiah Suarez on 6/12/20.
+//  Copyright © 2020 Isaiah Suarez. All rights reserved.
+//
+
+import XCTest
+@testable import Audio_Playground
+
+class Audio_PlaygroundTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
