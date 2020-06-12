30fd9469056ae96d56e6297649a5a22df0d2b229
Isaiah Suarez <isaiah.suarez@icloud.com>
2020-06-12 12:18:25 -0500

    App plays sound

--- Audio Playground/ViewController.swift
+++ Audio Playground/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    // MARK: IBActions
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        playAudio()
+    }
+    
+    func playAudio() {
+        
+        let path = Bundle.main.path(forResource: "example.m4a", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            // Couldn't load file :(
+        }
+    }
+    
 
 }
-
